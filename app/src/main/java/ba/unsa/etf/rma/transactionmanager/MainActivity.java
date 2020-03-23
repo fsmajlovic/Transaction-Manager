97b4e5568cd34c1edd5980155b98f4c990399a6a
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-03-23 13:47:04 +0100

    Arrows now change months.

--- app/src/main/java/ba/unsa/etf/rma/transactionmanager/MainActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/transactionmanager/MainActivity.java
@@ -2,12 +2,24 @@ package ba.unsa.etf.rma.transactionmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner filterSpinner;
+    private TextView monthTextView;
+    private ImageView arrowBackImageView;
+    private ImageView arrowForwardImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         filterSpinner = (Spinner) findViewById(R.id.filterSpinner);
+        monthTextView = (TextView) findViewById(R.id.monthTextView);
+        arrowBackImageView = (ImageView) findViewById(R.id.arrowBackwardImageView);
+        arrowForwardImageView = (ImageView) findViewById(R.id.arrowForwardImageView);
+
+        //Month regulation
+        final Calendar currentMonth = Calendar.getInstance();
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM");
+        monthTextView.setText(dateFormat.format(currentMonth.getTime()));
+
+        arrowBackImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentMonth.add(Calendar.MONTH, -1);
+                monthTextView.setText(dateFormat.format(currentMonth.getTime()));
+            }
+        });
+
+        arrowForwardImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentMonth.add(Calendar.MONTH, +1);
+                monthTextView.setText(dateFormat.format(currentMonth.getTime()));
+            }
+        });
+
+
+
 
     }
 }
