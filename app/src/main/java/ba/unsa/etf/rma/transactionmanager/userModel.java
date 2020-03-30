510539178bcd7815673f1bc4b4d93c1660256931
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-03-30 23:31:58 +0200

    More transactions added to model.

--- app/src/main/java/ba/unsa/etf/rma/transactionmanager/userModel.java
+++ app/src/main/java/ba/unsa/etf/rma/transactionmanager/userModel.java
@@ -25,92 +25,65 @@ public class userModel {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
         transactions.add( new Transaction(sdf.parse("16-01-2020"), 100,
-                "Electricity bill", Transaction.Type.REGULARPAYMENT, "Monthly bill for electricity.",
+                "Electricity expenses", Transaction.Type.REGULARPAYMENT, "Standard monthly" +
+                " bill for electricity expenses in January is $100. This amount goes to your power company. " +
+                "Total amount of $100 has been included in your Transaction Manager app.",
                 30, sdf.parse("15-02-2020")));
-        transactions.add( new Transaction(sdf.parse("17-02-2020"), 3125,
-                "Motorbike purchase", Transaction.Type.PURCHASE, "New Yamaha motorbike.",
+        transactions.add( new Transaction(sdf.parse("17-02-2020"), 4125,
+                "Motorbike purchase", Transaction.Type.PURCHASE, "New Yamaha motorbike " +
+                "bought at Yamaha Sarajevo motorbike store. Black color with the maximum speed of 220 km/h." +
+                "Total amount of $4125 has been included in your Transaction Manager app.",
                 0, null));
         transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "Regular salary", Transaction.Type.REGULARINCOME, "Monthly salary.",
+                "Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+                "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app. ",
                 30, sdf.parse("09-03-2020")));
-        transactions.add( new Transaction(sdf.parse("11-02-2020"), 150,
-                "Office subscription", Transaction.Type.REGULARPAYMENT,
-                "Yearly microsoft office subscription.",
-                365, sdf.parse("10-02-2021")));
         transactions.add( new Transaction(sdf.parse("15-03-2020"), 600,
                 "Freelancer project", Transaction.Type.INDIVIDUALINCOME,
-                "Payment for finishing an android app project on your Freelancer profile.",
+                "Paycheck for finishing several tasks for your Freelancer client: App " +
+                        "development, SQL database established, MD5 Hashing added to profile passwords. " +
+                        "Total amount of $600 has been included in your Transaction Manager app.",
                 0,null));
         transactions.add( new Transaction(sdf.parse("18-03-2020"), 500,
                 "Speeding ticket", Transaction.Type.INDIVIDUALPAYMENT,
-                "500$ speeding ticket violation.",
+                "Camera caught you violating the speed limit law. You have been speeding " +
+                        "with the speed of 100 km/h in the 70 km/h area. Destination: \"Zmaja od Bosne, Sarajevo\" " +
+                        "Total amount of $500 has been included in your Transaction Manager app.",
                 0,null));
 
-        transactions.add( new Transaction(sdf.parse("16-01-2020"), 100,
-                "Electricity bill", Transaction.Type.REGULARPAYMENT, "Monthly bill for electricity.",
-                30, sdf.parse("15-02-2020")));
-        transactions.add( new Transaction(sdf.parse("17-02-2020"), 3125,
-                "Motorbike purchase", Transaction.Type.PURCHASE, "New Yamaha motorbike.",
-                0, null));
-        transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "Regular salary", Transaction.Type.REGULARINCOME, "Monthly salary.",
-                30, sdf.parse("09-03-2020")));
+
+
         transactions.add( new Transaction(sdf.parse("11-02-2020"), 150,
-                "Office subscription", Transaction.Type.REGULARPAYMENT,
-                "Yearly microsoft office subscription.",
+                "MS Office", Transaction.Type.REGULARPAYMENT,
+                "Yearly Microsoft office subscription that allows you to use all of the " +
+                        "office features and programs. All of the updates have been included as well. " +
+                        "Total amount of $150 has been included in your Transaction Manager app.",
                 365, sdf.parse("10-02-2021")));
-        transactions.add( new Transaction(sdf.parse("15-03-2020"), 655,
+        transactions.add( new Transaction(sdf.parse("17-02-2020"), 700,
+                "ASUS monitor", Transaction.Type.PURCHASE, "New ASUS computer " +
+                "monitor purchase. ASUS ROG Swift PG348Q 34\" Curved Ultra-wide G-SYNC Gaming Monitor " +
+                "Total amount of 700 has been included in your Transaction Manager app.",
+                0, null));
+
+
+        //continue here
+        transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
+                "AirBNB", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+                "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app. ",
+                30, sdf.parse("09-03-2020")));
+        transactions.add( new Transaction(sdf.parse("15-03-2020"), 600,
                 "Freelancer project", Transaction.Type.INDIVIDUALINCOME,
-                "Payment for finishing an android app project on your Freelancer profile.",
+                "Paycheck for finishing several tasks for your Freelancer client: App " +
+                        "development, SQL database established, MD5 Hashing added to profile passwords. " +
+                        "Total amount of $600 has been included in your Transaction Manager app.",
                 0,null));
         transactions.add( new Transaction(sdf.parse("18-03-2020"), 500,
                 "Speeding ticket", Transaction.Type.INDIVIDUALPAYMENT,
-                "500$ speeding ticket violation.",
+                "Camera caught you violating the speed limit law. You have been speeding " +
+                        "with the speed of 100 km/h in the 70 km/h area. Destination: \"Zmaja od Bosne, Sarajevo\" " +
+                        "Total amount of $500 has been included in your Transaction Manager app.",
                 0,null));
 
-        transactions.add( new Transaction(sdf.parse("16-01-2020"), 100,
-                "Electricity bill", Transaction.Type.REGULARPAYMENT, "Monthly bill for electricity.",
-                30, sdf.parse("15-02-2020")));
-        transactions.add( new Transaction(sdf.parse("17-02-2020"), 3125,
-                "Motorbike purchase", Transaction.Type.PURCHASE, "New Yamaha motorbike.",
-                0, null));
-        transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "Regular salary", Transaction.Type.REGULARINCOME, "Monthly salary.",
-                30, sdf.parse("09-03-2020")));
-        transactions.add( new Transaction(sdf.parse("11-02-2020"), 150,
-                "Office subscription", Transaction.Type.REGULARPAYMENT,
-                "Yearly microsoft office subscription.",
-                365, sdf.parse("10-02-2021")));
-        transactions.add( new Transaction(sdf.parse("15-03-2020"), 620,
-                "Freelancer project", Transaction.Type.INDIVIDUALINCOME,
-                "Payment for finishing an android app project on your Freelancer profile.",
-                0,null));
-        transactions.add( new Transaction(sdf.parse("18-03-2020"), 500,
-                "Speeding ticket", Transaction.Type.INDIVIDUALPAYMENT,
-                "500$ speeding ticket violation.",
-                0,null));
-
-        transactions.add( new Transaction(sdf.parse("16-01-2020"), 100,
-                "Electricity bill", Transaction.Type.REGULARPAYMENT, "Monthly bill for electricity.",
-                30, sdf.parse("15-02-2020")));
-        transactions.add( new Transaction(sdf.parse("17-02-2020"), 3125,
-                "Motorbike purchase", Transaction.Type.PURCHASE, "New Yamaha motorbike.",
-                0, null));
-        transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "Regular salary", Transaction.Type.REGULARINCOME, "Monthly salary.",
-                30, sdf.parse("09-03-2020")));
-        transactions.add( new Transaction(sdf.parse("11-02-2020"), 150,
-                "Office subscription", Transaction.Type.REGULARPAYMENT,
-                "Yearly microsoft office subscription.",
-                365, sdf.parse("10-02-2021")));
-        transactions.add( new Transaction(sdf.parse("15-03-2020"), 619,
-                "Freelancer project", Transaction.Type.INDIVIDUALINCOME,
-                "Payment for finishing an android app project on your Freelancer profile.",
-                0,null));
-        transactions.add( new Transaction(sdf.parse("18-03-2020"), 500,
-                "Speeding ticket", Transaction.Type.INDIVIDUALPAYMENT,
-                "500$ speeding ticket violation.",
-                0,null));
 
 
     }
