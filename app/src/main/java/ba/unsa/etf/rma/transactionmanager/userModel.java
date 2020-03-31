961c119367a6ca201ea744cd72eb8e2d957d6226
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-03-31 16:42:13 +0200

    20 transactions in total edited by now.

--- app/src/main/java/ba/unsa/etf/rma/transactionmanager/userModel.java
+++ app/src/main/java/ba/unsa/etf/rma/transactionmanager/userModel.java
@@ -35,7 +35,7 @@ public class userModel {
                 "Total amount of $4125 has been included in your Transaction Manager app.",
                 0, null));
         transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+                "February Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
                 "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app. ",
                 30, sdf.parse("09-03-2020")));
         transactions.add( new Transaction(sdf.parse("15-03-2020"), 600,
@@ -50,9 +50,6 @@ public class userModel {
                         "with the speed of 100 km/h in the 70 km/h area. Destination: \"Zmaja od Bosne, Sarajevo\" " +
                         "Total amount of $500 has been included in your Transaction Manager app.",
                 0,null));
-
-
-
         transactions.add( new Transaction(sdf.parse("11-02-2020"), 150,
                 "MS Office", Transaction.Type.REGULARPAYMENT,
                 "Yearly Microsoft office subscription that allows you to use all of the " +
@@ -64,25 +61,73 @@ public class userModel {
                 "monitor purchase. ASUS ROG Swift PG348Q 34\" Curved Ultra-wide G-SYNC Gaming Monitor " +
                 "Total amount of 700 has been included in your Transaction Manager app.",
                 0, null));
-
-
-        //continue here
-        transactions.add( new Transaction(sdf.parse("10-02-2020"), 3500,
-                "AirBNB", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+        transactions.add( new Transaction(sdf.parse("10-03-2020"), 3500,
+                "March Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
                 "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app. ",
-                30, sdf.parse("09-03-2020")));
-        transactions.add( new Transaction(sdf.parse("15-03-2020"), 600,
-                "Freelancer project", Transaction.Type.INDIVIDUALINCOME,
-                "Paycheck for finishing several tasks for your Freelancer client: App " +
-                        "development, SQL database established, MD5 Hashing added to profile passwords. " +
-                        "Total amount of $600 has been included in your Transaction Manager app.",
+                30, sdf.parse("09-04-2020")));
+        transactions.add( new Transaction(sdf.parse("01-04-2020"), 100,
+                "Airbnb host", Transaction.Type.INDIVIDUALINCOME,
+                "Luxury double room near center of Sarajevo" +
+                        " including private bathroom and a shared kitchen has been rented for 4 days by you. " +
+                        "Total amount of $100 has been included in your Transaction Manager app.",
                 0,null));
-        transactions.add( new Transaction(sdf.parse("18-03-2020"), 500,
-                "Speeding ticket", Transaction.Type.INDIVIDUALPAYMENT,
-                "Camera caught you violating the speed limit law. You have been speeding " +
-                        "with the speed of 100 km/h in the 70 km/h area. Destination: \"Zmaja od Bosne, Sarajevo\" " +
-                        "Total amount of $500 has been included in your Transaction Manager app.",
+        transactions.add( new Transaction(sdf.parse("18-03-2020"), 40,
+                "Drone shipment", Transaction.Type.INDIVIDUALPAYMENT,
+                "You have ordered a new drone via Ebay from China. Takeoff Weight: " +
+                        "249 g, Diagonal Distance: 213 mm, Max Flight Time 30 minutes (measured while " +
+                        "flying at 14 kph in windless conditions). You have to pay additional $40 for shipment. " +
+                        "Total amount of $40 has been included in your Transaction Manager app.",
                 0,null));
+
+
+
+        transactions.add( new Transaction(sdf.parse("22-03-2020"), 30,
+                "Water expenses", Transaction.Type.REGULARPAYMENT, "Standard monthly" +
+                " bill for water expenses in is $30. This amount goes to your water company. " +
+                "Total amount of $30 has been included in your Transaction Manager app.",
+                30, sdf.parse("21-04-2020")));
+        transactions.add( new Transaction(sdf.parse("17-02-2020"), 650,
+                "Playstation purchase", Transaction.Type.PURCHASE, "New Playstatin 4" +
+                "bought at Target Sarajevo videogame store. Specs: CPU : x86-64 AMD “Jaguar”, 8 cores, " +
+                "Storage size: 500GB, 1TB. Total amount of $650 has been included in your Transaction Manager app.",
+                0, null));
+        transactions.add( new Transaction(sdf.parse("10-01-2020"), 3500,
+                "January Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+                "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app. ",
+                30, sdf.parse("09-02-2020")));
+        transactions.add( new Transaction(sdf.parse("11-01-2020"), 200,
+                "Youtube income", Transaction.Type.INDIVIDUALINCOME,
+                "Paycheck from youtube for your \"Visual Studio Tutorials\" profile." +
+                        " Total amount of $200 has been included in your Transaction Manager app.",
+                0,null));
+        transactions.add( new Transaction(sdf.parse("01-01-2020"), 360,
+                "Boarding ticket", Transaction.Type.INDIVIDUALPAYMENT,
+                "Boarding ticket Sarajevo(SJJ) - Florenta(FLR) booked for 05.06.2020 date and " +
+                        "return date 15.06.2020. Total amount of @360 has been included in your Transaction Manager app.",
+                0,null));
+        transactions.add( new Transaction(sdf.parse("15-03-2020"), 40,
+                "Internet expenses", Transaction.Type.REGULARPAYMENT,
+                "Bill for internet expenses in March is $40. This amount goes to BHTelecom internet provider. " +
+                        "Total amount of $40 has been included in your Transaction Manager app.",
+                365, sdf.parse("14-04-2021")));
+        transactions.add( new Transaction(sdf.parse("01-01-2020"), 140,
+                "RHCP tickets", Transaction.Type.PURCHASE, "Two tickets for the Red Hot Chili Peppers " +
+                "performance in Firenze. Date: June, 13th. Total amount of $140 has been included in your Transaction Manager app.",
+                0, null));
+        transactions.add( new Transaction(sdf.parse("10-04-2020"), 3500,
+                "April Salary", Transaction.Type.REGULARINCOME, "Monthly salary income " +
+                "from AppDev CO. Total amount of $3500 has been included in your Transaction Manager app.",
+                30, sdf.parse("09-05-2020")));
+        transactions.add( new Transaction(sdf.parse("12-04-2020"), 1380,
+                "Samsung Galaxy Z Flip", Transaction.Type.PURCHASE, "New Samsung Galaxy mobile phone. " +
+                "Inovative Foldable Dynamic AMOLED capacitive touchscreen, 16M colors. " +
+                "Total amount of $1380 has been included in your Transaction Manager app.",
+                0, null));
+        transactions.add( new Transaction(sdf.parse("07-02-2020"), 1200,
+                "Fender Stratocaster", Transaction.Type.PURCHASE, "New Fender Custom 57 Stratocaster 3" +
+                " Color Sunburst with a Marshal studio amp. Total amount of $1200 has been included in your Transaction Manager app.",
+                0, null));
+
 
 
 
