77965eb6a67e0405373e310a19766814f20f99db
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-04-12 13:34:57 +0200

    Total limit and month limit are now editable in Budget Fragment.

--- app/src/main/java/ba/unsa/etf/rma/transactionmanager/BudgetFragment.java
+++ app/src/main/java/ba/unsa/etf/rma/transactionmanager/BudgetFragment.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rma.transactionmanager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +22,8 @@ public class BudgetFragment extends Fragment {
     private EditText budgetEditText;
     private EditText totalLimitEditText;
     private EditText monthLimitEditText;
+    private TextView saveTextView;
+    private BudgetPresenter presenter;
     private boolean budgetVal = true, monthVal = true, totalVal = true;
 
 
@@ -30,15 +35,16 @@ public class BudgetFragment extends Fragment {
         budgetEditText = fragmentView.findViewById(R.id.BudgetEditText);
         totalLimitEditText = fragmentView.findViewById(R.id.TotalLimitEditText);
         monthLimitEditText = fragmentView.findViewById(R.id.monthLimitEditText);
+        saveTextView = fragmentView.findViewById(R.id.saveTextView);
 
         try {
-            BudgetPresenter presenter = new BudgetPresenter();
+            presenter = new BudgetPresenter();
             budgetEditText.setText(String.valueOf(presenter.getInteractor().getBudget()));
+            monthLimitEditText.setText(String.valueOf(presenter.getInteractor().getMonthLimit()));
+            totalLimitEditText.setText(String.valueOf(presenter.getInteractor().getTotalLimit()));
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-
 
 
         budgetEditText.addTextChangedListener(new TextWatcher() {
@@ -50,10 +56,10 @@ public class BudgetFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.toString().matches("-?\\d+(\\.\\d+)?")) {
-                    budgetEditText.setBackgroundColor(Color.parseColor("#169617"));
+                    budgetEditText.setTextColor(Color.parseColor("#B8A228"));
                     budgetVal = true;
                 } else {
-                    budgetEditText.setBackgroundColor(Color.parseColor("#B41D1D"));
+                    budgetEditText.setTextColor(Color.parseColor("#B41D1D"));
                     budgetVal = false;
                 }
             }
@@ -63,6 +69,76 @@ public class BudgetFragment extends Fragment {
 
             }
         });
+
+        monthLimitEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().matches("-?\\d+(\\.\\d+)?")) {
+                    monthLimitEditText.setTextColor(Color.parseColor("#B8A228"));
+                    monthVal = true;
+                } else {
+                    monthLimitEditText.setTextColor(Color.parseColor("#B41D1D"));
+                    monthVal = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        totalLimitEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().matches("-?\\d+(\\.\\d+)?")) {
+                    totalLimitEditText.setTextColor(Color.parseColor("#B8A228"));
+                    totalVal = true;
+                } else {
+                    totalLimitEditText.setTextColor(Color.parseColor("#B41D1D"));
+                    totalVal = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        saveTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(budgetVal && monthVal && totalVal){
+                    presenter.setBudget(Double.valueOf(budgetEditText.getText().toString()));
+                    presenter.setMonthLimit(Double.valueOf(monthLimitEditText.getText().toString()));
+                    presenter.setTotalLimit(Double.valueOf(totalLimitEditText.getText().toString()));
+                }
+                else {
+                    new AlertDialog.Builder(getActivity(), R.style.AlertDialog)
+                            .setTitle("Changes")
+                            .setMessage("Seems like some of your changes might be wrong.(Red color indicates an incorrect change).")
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            }
+        });
+
 
         return fragmentView;
     }
